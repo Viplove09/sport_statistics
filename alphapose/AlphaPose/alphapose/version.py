bd76c186e449a91138e50592a7f00d3586bfb124
vips-2219 <chaudhary.viplove94@gmail.com>
2021-09-01 09:41:29 +0530

    first

---
+++ alphapose/AlphaPose/alphapose/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Sun Jul 11 21:09:56 2021
+
+__version__ = '0.3.0+d75469f'
+short_version = '0.3.0'
---
+++ alphapose/AlphaPose/build/lib/alphapose/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Thu Jun 10 16:38:52 2021
+
+__version__ = '0.3.0+d75469f'
+short_version = '0.3.0'
